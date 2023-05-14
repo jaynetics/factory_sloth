c0e1d96a3b134a82db87cbba15dd06144fd829bc
Janosch Müller <janosch84@gmail.com>
2023-05-14 22:09:37 +0200

    Fix for simple shells

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [Unreleased]
 
+### Fixed
+
+- Fixed shelling out on linuxes with simple shells
+
 ## [1.0.0] - 2023-05-14
 
 - Initial release
--- lib/factory_sloth/code_mod.rb
+++ lib/factory_sloth/code_mod.rb
@@ -55,7 +55,7 @@ class FactorySloth::CodeMod
     tempfile.write(spec_code)
     tempfile.close
     path = [tempfile.path, line].compact.map(&:to_s).join(':')
-    result = !!system("bundle exec rspec #{path} --fail-fast &> /dev/null")
+    result = !!system("bundle exec rspec #{path} --fail-fast 1>/dev/null 2>&1")
     tempfile.unlink
     result
   end
