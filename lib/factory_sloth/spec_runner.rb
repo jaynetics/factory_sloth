beeafb06b3cf598c9dccc6b26b4d630d811b626a
Janosch Müller <janosch84@gmail.com>
2023-05-17 19:28:07 +0200

    Fix build speed

--- lib/factory_sloth/spec_runner.rb
+++ lib/factory_sloth/spec_runner.rb
@@ -3,7 +3,7 @@ require 'tmpdir'
 module FactorySloth::SpecRunner
   def self.call(spec_path, spec_code, line: nil)
     Dir.mktmpdir do |tmpdir|
-      path = File.join(tmpdir, spec_path)
+      path = File.join(tmpdir, File.expand_path(spec_path))
       FileUtils.mkdir_p(File.dirname(path))
       File.write(path, spec_code)
       path_arg = [path, line].compact.map(&:to_s).join(':')
