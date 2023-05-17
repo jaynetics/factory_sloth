9f2b87a75923af8aa9a69f53a47ac0a7cf0b259d
Janosch Müller <janosch84@gmail.com>
2023-05-17 18:57:20 +0200

    Test speed

--- lib/factory_sloth/spec_runner.rb
+++ lib/factory_sloth/spec_runner.rb
@@ -2,9 +2,11 @@ require 'tmpdir'
 
 module FactorySloth::SpecRunner
   def self.call(spec_path, spec_code, line: nil)
+    puts 'spec_path', spec_path
     Dir.mktmpdir do |tmpdir|
       path = File.join(tmpdir, spec_path)
-      FileUtils.mkdir_p(File.dirname(path))
+      puts 'File.dirname(path)', File.dirname(File.expand_path((path)))
+      FileUtils.mkdir_p(File.dirname(File.expand_path(path)))
       File.write(path, spec_code)
       path_arg = [path, line].compact.map(&:to_s).join(':')
       !!system("bundle exec rspec #{path_arg} --fail-fast 1>/dev/null 2>&1")
