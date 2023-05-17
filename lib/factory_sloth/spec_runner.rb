efb185ca7364145fa862f8263bc4ae5b1b6f1847
Janosch Müller <janosch84@gmail.com>
2023-05-17 19:38:46 +0200

    Test 5

--- lib/factory_sloth/spec_runner.rb
+++ lib/factory_sloth/spec_runner.rb
@@ -2,8 +2,10 @@ require 'tmpdir'
 
 module FactorySloth::SpecRunner
   def self.call(spec_path, spec_code, line: nil)
+    puts 'spec_path', spec_path
     Dir.mktmpdir do |tmpdir|
       path = File.join(tmpdir, File.expand_path(spec_path))
+      puts 'File.dirname(path)', File.dirname(path)
       FileUtils.mkdir_p(File.dirname(path))
       File.write(path, spec_code)
       path_arg = [path, line].compact.map(&:to_s).join(':')
