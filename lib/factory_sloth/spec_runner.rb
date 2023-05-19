c03150c56aebaf44cbef1ec0240c79dd762e8ab3
Janosch Müller <janosch84@gmail.com>
2023-05-19 19:53:35 +0200

    Reduce disk IO a bit ...
    by not deleting and recreating all intermediate directories for every spec

--- lib/factory_sloth/spec_runner.rb
+++ lib/factory_sloth/spec_runner.rb
@@ -3,16 +3,20 @@ require 'tmpdir'
 
 module FactorySloth::SpecRunner
   def self.call(spec_path, spec_code, line: nil)
-    Dir.mktmpdir do |tmpdir|
-      path = File.join(tmpdir, spec_path)
-      FileUtils.mkdir_p(File.dirname(path))
-      File.write(path, spec_code)
-      path_arg = [path, line].compact.map(&:to_s).join(':')
-      command = "bundle exec rspec #{path_arg} --fail-fast --order defined 2>&1"
-      _output, status = Open3.capture2(command)
-      status
-    end
+    path = File.join(tmpdir, spec_path)
+    FileUtils.mkdir_p(File.dirname(path))
+    File.write(path, spec_code)
+    path_arg = [path, line].compact.map(&:to_s).join(':')
+    command = "bundle exec rspec #{path_arg} --fail-fast --order defined 2>&1"
+    _output, status = Open3.capture2(command)
+    status
   end
+
+  def self.tmpdir
+    @tmpdir ||= begin
+      dir = Dir.mktmpdir('factory_sloth-')
+      at_exit { FileUtils.remove_entry(dir) if File.exist?(dir) }
+      dir
     end
   end
 end
