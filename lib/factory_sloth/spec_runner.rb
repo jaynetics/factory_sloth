06b1e71128c51ac429bec1d9c11ea1e359d4eed5
Janosch Müller <janosch84@gmail.com>
2023-05-19 10:03:03 +0200

    Run in order for reproducible results

--- lib/factory_sloth/spec_runner.rb
+++ lib/factory_sloth/spec_runner.rb
@@ -7,7 +7,7 @@ module FactorySloth::SpecRunner
       FileUtils.mkdir_p(File.dirname(path))
       File.write(path, spec_code)
       path_arg = [path, line].compact.map(&:to_s).join(':')
-      !!system("bundle exec rspec #{path_arg} --fail-fast 1>/dev/null 2>&1")
+      !!system("bundle exec rspec #{path_arg} --fail-fast --order defined 1>/dev/null 2>&1")
     end
   end
 end
